267e23dc63be6c10dd9d1015dd13dfaddff04796
hillarydunkley16 <hvgdunkley@gmail.com>
2022-04-11 23:55:00 -0400

    added views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html()
+    end
+    #@data = Datum.all
+  end
+  def show
+    @data = Datum.find(params[:id])
+  end
+
+  def protein
+    respond_to do |format|
+      format.html {render :protein, locals: {feedback: {}}}
+    end
+  end
+
   def input_protein
     required = [:input1, :input2, :input3, :input4, :input5, :reply]
     form_complete = true
@@ -18,19 +34,83 @@ class HomeController < ApplicationController
       format.html {render :protein, locals: {status_msg: form_status_msg, feedback: params}}
     end
   end
-  def index
+
+  def veg
     respond_to do |format|
-      format.html()
+      format.html {render :veg}
     end
-    #@data = Datum.all
-  end
-  def show
-    @data = Datum.find(params[:id])
   end
 
-  def protein
+  def input_veg
+    required = [:input1, :input2, :input3, :input4, :input5]
+    form_complete = true
+    required.each do |f|
+      if params.has_key? f and not params[f].blank?
+        #do nothing
+      else
+        form_complete = false
+      end
+    end 
+    if form_complete
+      form_status_msg = "Great. Let's continue!"
+    else
+      form_status_msg = "Please fill in all fields."
+    end
     respond_to do |format|
-      format.html {render :protein, locals: [feedback: {}}
+      format.html {render :veg, locals: {status_msg: form_status_msg, feedback: params}}
     end
   end
+
+  def aromatics
+    respond_to do |format|
+      format.html {render :aromatics}
+    end
+  end
+
+  def input_aromatics
+    required = [:input1, :input2, :input3, :input4, :input5]
+    form_complete = true
+    required.each do |f|
+      if params.has_key? f and not params[f].blank?
+        #do nothing
+      else
+        form_complete = false
+      end
+    end 
+    if form_complete
+      form_status_msg = "Great. Let's continue!"
+    else
+      form_status_msg = "Please fill in all fields."
+    end
+    respond_to do |format|
+      format.html {render :aromatics, locals: {status_msg: form_status_msg, feedback: params}}
+    end
+  end
+
+  def oils
+    respond_to do |format|
+      format.html {render :oils}
+    end
+  end
+  
+  def input_oils
+    required = [:input1, :input2, :input3, :input4, :input5]
+    form_complete = true
+    required.each do |f|
+      if params.has_key? f and not params[f].blank?
+        #do nothing
+      else
+        form_complete = false
+      end
+    end 
+    if form_complete
+      form_status_msg = "Great. Let's continue!"
+    else
+      form_status_msg = "Please fill in all fields."
+    end
+    respond_to do |format|
+      format.html {render :oils, locals: {status_msg: form_status_msg, feedback: params}}
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
   #get 'protein', to: 'home#protein', as: 'protein'
   root "home#index"
+
   post 'protein', to: 'home#input_protein', as: 'input_protein'
+
+  post 'veg', to: 'home#input_veg', as: 'input_veg'
+
+  post 'aromatics', to: 'home#input_aromatics', as: 'input_aromatics'
+
+  post 'oils', to: 'home#input_oils', as: 'input_oils'
+
   get 'index', to: 'home#index', as: 'index'
+
   get 'protein', to: 'home#protein', as: 'protein'
+
+  get 'veg', to: 'home#veg', as: 'veg'
+
+  get 'aromatics', to: 'home#aromatics', as: 'aromatics'
+
+  get 'oils', to: 'home#oils', as: 'oils'
+
   #get 'input/:action' to 'input#show'
   get "/home", to: "home#index"
   get "/home/:id", to: "home#show"
